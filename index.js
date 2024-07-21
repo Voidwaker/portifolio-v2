e24e9b8a02884b7afd1e0f810fb231753468add1
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2024-07-21 18:13:26 +0200

    added the right content to the diffrent "boxes
    added the right content for the right boxes under the projects category, also added some better content about myself. added a fast contact form (not added css for that yet.

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("myBtn").style.display = "block";
-  } else {
-    document.getElementById("myBtn").style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
 }
-
-
 
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
 }
 
+  
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault();
-    
         document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
+            behavior: 'smooth'
         });
     });
-    });
+});
